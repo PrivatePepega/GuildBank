defa0c4de24b4f42fd0ce1093238b78920bc019c
PrivatePepega <privatepepega@yahoo.com>
2025-09-08 20:13:30 -0400

    setting up contract address database

--- src/utils/contractAddressHardhat.js
+++ src/utils/contractAddressHardhat.js
@@ -31,37 +31,3 @@ export const contractAddresses = {
   Burner: GuildBankDAOBurner,
   Ballot: GuildBankGovernorBallotAddress,
 };
-
-
-// 🧱 GameBank contract addresses for Hardhat deployment
-
-// Individual contract addresses
-export const GameBankSourceAddress = "0x5eb3Bc0a489C5A8288765d2336659EbCA68FCd00";
-export const GameBankMoneyAddress = "0x36C02dA8a0983159322a80FFE9F24b1acfF8B570";
-export const GameBankTimeLockAddress = "0x809d550fca64d94Bd9F66E60752A544199cfAC3D";
-export const GameBankGovernorAddress = "0x4c5859f0F772848b2D91F1D83E2Fe57935348029";
-export const GameBankGovernorElectionAddress = "0x1291Be112d480055DaFd8a610b7d1e203891C274";
-export const GameBankGovernorBallotAddress = "0x5f3f1dBD7B74C6B46e8c44f98792A1dAf8d69154";
-export const GameBankBoDAddress = "0xb7278A61aa25c888815aFC32Ad3cC52fF24fE575";
-export const GameBankBoDTreasuryAddress = "0xCD8a1C3ba11CF5ECfa6267617243239504a98d90";
-export const GameBankGovernorTreasuryAddress = "0x82e01223d51Eb87e16A03E24687EDF0F294da6f1";
-export const GameBankBurnerAddress = "0x2bdCC0de6bE1f7D2ee689a0342D76F52E8EFABa3";
-export const GameBankAccessManagerAddress = "0x7969c5eD335650692Bc04293B07F5BF2e7A673C0";
-
-
-// Consolidated contract addresses object
-export const contractAddressesGame = {
-  Source: GameBankSourceAddress,
-  Money: GameBankMoneyAddress,
-  TimeLock: GameBankTimeLockAddress,
-  Governor: GameBankGovernorAddress,
-  GovernorElection: GameBankGovernorElectionAddress,
-  Ballot: GameBankGovernorBallotAddress,
-  BoD: GameBankBoDAddress,
-  BoDTreasury: GameBankBoDTreasuryAddress,
-  GovernorTreasury: GameBankGovernorTreasuryAddress,
-  Burner: GameBankBurnerAddress,
-  AccessManager: GameBankAccessManagerAddress,
-};
-
-
