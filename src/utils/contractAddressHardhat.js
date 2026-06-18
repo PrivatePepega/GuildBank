5b0eed398ea7ad77329c430f2766fd7795399d58
PrivatePepega <privatepepega@yahoo.com>
2026-06-18 14:53:00 -0400

    updaint contracts ever again

--- src/utils/contractAddressHardhat.js
+++ src/utils/contractAddressHardhat.js
@@ -1,15 +1,15 @@
 // 🧱 Updated contract addresses for Hardhat deployment
-export const GuildBankSourceAddress = "0x0b06FC62BB9F3A7849aD48eD04974B517d11a50C";
-export const GuildBankMoneyAddress = "0x50AC269382a3c1Ad9e4086609a02b153D7b54ee6";
-export const GuildBankPassportAddress = "0xc36861A4396Ed8406Ec45B412C1bfA71d54B5a07";
-export const GuildBankTimeLockAddress = "0x72AE508d5575B956C60794288A54cFc6F6b680F4";
-export const GuildBankGovernorAddress = "0xc0893a32fa9bbc799D76Bb5FC0A4774D0F622269";
-export const GuildBankGovernorElectionAddress = "0x07d2E9C761270D8477951e2b4DdF754aa5Cb128F";
-export const GuildBankGovernorBallotAddress = "0x71620F5B02036496BADDE51E9D6E989F5394023B";
-export const GuildBankBoDAddress = "0x6791E4e6E37a25C53F84D73c2617814C928b7153";
-export const GuildBankBoDTreasuryAddress = "0x0A9FB7ec81e67FC20e4f70d8A0768a711f9C9b88";
-export const GuildBankGovernorTreasuryAddress = "0x3Bd5eeA0664Fd4A2e026fDB761203D15aC976255";
-export const GuildBankAccessManagerAddress = "0xbAC3aCccd01452B992ABFE072127E23d6b1bf4C0";
+export const GuildBankSourceAddress = "0x5A3946b8Aa79B6d7fcf0C621Cd1486aAa7DcAd37";
+export const GuildBankMoneyAddress = "0x5C069fB31aFcF1fE0AAD58BbBe20C19ebb8E18bC";
+export const GuildBankPassportAddress = "0x1B621FaA831d9ec331514635056C70bc388e028b";
+export const GuildBankTimeLockAddress = "0x5445182d418B677065A5927f017BEd14c94f99D4";
+export const GuildBankGovernorAddress = "0x5F46383F77C67181803d4c425546D5e9ECD9B47B";
+export const GuildBankGovernorElectionAddress = "0xE89f569001C1899Eb1c917686A8DbDe82Ee37c16";
+export const GuildBankGovernorBallotAddress = "0x03fC7816C2Fd7a90F11d7B3d8d3b0E9F4EAE1F30";
+export const GuildBankBoDAddress = "0xCfE8779B9e85d34b15fbF7285d04525eD60DaA1B";
+export const GuildBankBoDTreasuryAddress = "0x5DfA17b46780836F908E9b977CfCF141600723a8";
+export const GuildBankGovernorTreasuryAddress = "0x0B77e8CC950E611c780f5A828F715aC30a54F9Bd";
+export const GuildBankAccessManagerAddress = "0x18776a9ceEa4FfB75863977d7c1838AB9d5b0337";
 
 export const contractAddresses = {
   Source: GuildBankSourceAddress,
